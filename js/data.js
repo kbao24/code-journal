ae14c2b2ccf3dd3af851e5d063b091ca38e635ba
Kevin <kevbao24@gmail.com>
2023-11-14 23:06:20 +0000

    In data.js, add a 'beforeunload' event listener to the window object that serializes the data model as JSON and stores the JSON in localStorage.

--- js/data.js
+++ js/data.js
@@ -1,8 +1,16 @@
 /* exported data */
 
-let data = {
+const data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1,
 };
+export default data;
+
+function saveData() {
+  const jsonData = JSON.stringify(data);
+  localStorage.setItem('data', jsonData);
+}
+
+window.addEventListener('beforeunload', saveData);
--- js/main.js
+++ js/main.js
@@ -1,6 +1,38 @@
-const photoURL = document.getElementById('photo-url');
-const photoPreview = document.querySelector('.entry-image-preview');
+import data from './data.js';
+// input event listener
+document.addEventListener('DOMContentLoaded', function () {
+  const photoURL = document.getElementById('photo-url');
+  const imgPreview = document.querySelector('.entry-image');
 
-photoURL.addEventListener('input', function () {
-  photoPreview.src = this.value;
+  photoURL.addEventListener('input', function () {
+    const imgURL = this.value;
+    imgPreview.src = imgURL;
+  });
+});
+
+// submit event listener
+const entryForm = document.querySelector('form');
+// console.log(entryForm);
+
+entryForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const title = document.getElementById('title').value;
+  const photoURLInput = document.getElementById('photo-url').value;
+  const notes = document.getElementById('bottom').value;
+
+  const formData = {
+    title,
+    photoURLInput,
+    notes,
+  };
+
+  data.nextEntryId++;
+
+  data.entries.unshift(formData);
+
+  const imgPreview = document.querySelector('.entry-image');
+  imgPreview.src = 'images/placeholder-image-square.jpg';
+
+  entryForm.reset();
 });
