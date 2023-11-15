d5a0d2960b3d5350a422636eea6cf92c4a54ce8d
Kevin <kevbao24@gmail.com>
2023-11-15 19:36:50 +0000

    At the bottom of data.js, include a conditional statement that:

--- js/data.js
+++ js/data.js
@@ -1,15 +1,30 @@
 /* exported data */
 
-const data = {
+let data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1,
 };
 
+// event listener
 function saveData() {
   const jsonData = JSON.stringify(data);
   localStorage.setItem('data', jsonData);
 }
 
 window.addEventListener('beforeunload', saveData);
+
+// conditional statement
+const storedData = localStorage.getItem('myData');
+
+if (storedData) {
+  data = JSON.parse(storedData);
+} else {
+  data = {
+    view: 'entry-form',
+    entries: [],
+    editing: null,
+    nextEntryId: 1,
+  };
+}
