4ccd031f0c32d4999bc33b2cd9eaf2cff5378b27
Kevin <kevbao24@gmail.com>
2023-11-21 15:58:19 +0000

    Create an event listener on the document which listens for the DOMContentLoaded event. In the callback function for that event, loop through the data.entries array and generate a DOM tree for each entry in the array, and append that DOM tree to the unordered list.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /* global data */
 
+// issue 1
 document.addEventListener('DOMContentLoaded', function () {
   const photoURL = document.getElementById('photo-url');
   const imgPreview = document.querySelector('.entry-image');
@@ -35,4 +36,45 @@ entryForm.addEventListener('submit', function (event) {
   imgPreview.src = 'images/placeholder-image-square.jpg';
 
   entryForm.reset();
+});
+
+// issue 2
+function renderEntry(entry) {
+  const listItem = document.createElement('li');
+  listItem.classList.add('entry');
+
+  const entryContent = document.createElement('div');
+  entryContent.classList.add('entry-content');
+
+  const entryImage = document.createElement('img');
+  entryImage.src = entry.imageURL;
+  entryImage.alt = entry.title;
+
+  const entryDetails = document.createElement('div');
+  entryDetails.classList.add('entry-details');
+
+  const entryTitle = document.createElement('h3');
+  entryTitle.textContent = entry.title;
+
+  const entryDescription = document.createElement('p');
+  entryDescription.textContent = entry.description;
+
+  entryDetails.appendChild(entryTitle);
+  entryDetails.appendChild(entryDescription);
+
+  entryContent.appendChild(entryImage);
+  entryContent.appendChild(entryDetails);
+
+  listItem.appendChild(entryContent);
+
+  return listItem;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const entriesList = document.querySelector('[data-view="entries"]');
+
+  data.entries.forEach((entry) => {
+    const entryElement = renderEntry(entry);
+    entriesList.appendChild(entryElement);
+  });
 });
